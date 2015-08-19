7cab256e7c57e033da0d6a3559b34c7911e13dc3
we2_deployer <we2@edss.ee>
2015-08-19 16:33:15 +0300

    function save

--- world.js
+++ world.js
@@ -7,13 +7,18 @@ function World(){
   function packKey(objId,attrName){ return objId+'.'+attrName }
   function packReverseKey(attrName,value){ return attrName+'='+value }
 
+  function Error(arg,msg){
+    if(!msg) msg = 'Error';
+    this.toString = function(){ return msg+' - '+arg.toString()}
+  }
+
   function Prop(attrName){
       function run(objIds,args){ return getOrSet(key,flat,unflat,objIds,args) }
       function key(objId){ return packKey(objId,attrName) }
       function unflat(values){
           if(values.length==0) return "";
           if(values.length==1) return values[0] + "";
-          throw values
+          throw new Error(values)
       }
       function flat(value){ return value + "" }
       run.rev = Rev(attrName)
@@ -46,15 +51,15 @@ function World(){
           })
           return Obj(valueIds)
       }
-      function flat(value){ throw "can not flat" }
+      function flat(value){ throw new Error(value,"can not flat") }
       run.toString = function(){ return "Rev<"+attrName+">" }
       return run
   }
 
   function id(objIds,args){
-      if(args.length > 0) throw args
+      if(args.length > 0) throw new Error(args)
       if(objIds.length==1) return objIds[0]
-      throw objIds
+      throw new Error( objIds )
   }
 
   function ids(objIds,args){
@@ -64,16 +69,16 @@ function World(){
   function getOrSet(key,flat,unflat,objIds,args){
     function set(){
       if(objIds.length==1) return tx.changes[key(objIds[0])] = flat(args[0]);
-      throw objIds
+      throw new Error( objIds )
     }
     if(args.length > 0){
       if(!tx) return rwTx(function(){ set() })
       if(tx.changes) return set()
-      throw "ro tx"
+      throw new Error(tx, "ro tx")
     }
     return unflat(objIds.map(function(objId){
         var k = key(objId)
-        if(!tx) throw "out of tx"
+        if(!tx) throw new Error(tx, "out of tx")
         return world[k]
     }))
   }
@@ -95,7 +100,7 @@ function World(){
   function where(value){ return Obj([value]) }
 
   function doTx(ini,f){
-    if(tx) throw "nested tx is not supported"
+    if(tx) throw new Error(tx,"nested tx is not supported")
     tx = { changes:ini }
     try {
       return f()
@@ -103,7 +108,7 @@ function World(){
       tx = null
     }
   }
-  function roTx(f){ doTx(null,f) }
+  function roTx(f){ return doTx(null,f) }
   function rwTx(f){
       function index(key,on){
           var dot = key.indexOf(".")
@@ -124,6 +129,26 @@ function World(){
       })
   }
 
+  function exportEach(f){
+    var keys=[];
+    for(var key in world){
+      var equ = key.indexOf("=");
+      if(equ !== -1) continue;
+      keys.push(key);
+    };
+    keys.sort();
+    var len = keys.length;
+
+    for(var i = 0; i < len; i++){
+      var dot = keys[i].indexOf(".");
+      var objId = keys[i].substr(0,dot);
+      var attrName = keys[i].substr(dot+1);
+      var value = world[keys[i]];
+
+      f(objId,attrName,value);
+    }
+  }
+
   function dump(){ console.log(world) }
 
   return {
@@ -134,6 +159,7 @@ function World(){
     where: where,
     roTx: roTx,
     rwTx: rwTx,
+    exportEach: exportEach,
     dump: dump
   };
 }
